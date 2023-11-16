860e5bfd7a692bc6a9fdf4808d50906c8771a0a4
Yuchan Lee <lasuillard@gmail.com>
2023-11-16 11:18:05 +0000

    test: Write tests

---
+++ tests/conftest.py
@@ -0,0 +1,18 @@
+from __future__ import annotations
+
+import subprocess
+from pathlib import Path
+from typing import TYPE_CHECKING
+
+import pytest
+
+if TYPE_CHECKING:
+    from _pytest._py.path import LocalPath
+
+
+# https://github.com/pre-commit/pre-commit-hooks/blob/main/tests/conftest.py
+@pytest.fixture()
+def temp_git_dir(tmpdir: LocalPath) -> Path:
+    git_dir = Path(tmpdir) / "git"
+    subprocess.Popen(["git", "init", "--", str(git_dir)])  # noqa: S603, S607
+    return git_dir
---
+++ tests/test_check_directory_structure.py
@@ -0,0 +1,58 @@
+from pathlib import Path
+
+from src.check_directory_structure import check_directory_structure
+
+
+def test_empty_dir(temp_git_dir: Path) -> None:
+    assert (
+        check_directory_structure(
+            source=temp_git_dir / "src",
+            target=temp_git_dir / "tests",
+        )
+        == 0
+    )
+
+
+def test_only_src_dir_populated(temp_git_dir: Path) -> None:
+    filenames = [
+        "src/__init__.py",
+        "src/main.py",
+        "src/util/logger.py",
+        "src/util/telemetry/traces.py",
+    ]
+    for filename in filenames:
+        file = temp_git_dir / filename
+        file.parent.mkdir(parents=True, exist_ok=True)
+        file.touch()
+
+    assert (
+        check_directory_structure(
+            source=temp_git_dir / "src",
+            target=temp_git_dir / "tests",
+        )
+        == 1
+    )
+
+
+def test_dir_fully_populated(temp_git_dir: Path) -> None:
+    filenames = [
+        "src/__init__.py",
+        "src/main.py",
+        "src/util/logger.py",
+        "src/util/telemetry/traces.py",
+        "tests/test_main.py",
+        "tests/util/test_logger.py",
+        "tests/util/telemetry/test_traces.py",
+    ]
+    for filename in filenames:
+        file = temp_git_dir / filename
+        file.parent.mkdir(parents=True, exist_ok=True)
+        file.touch()
+
+    assert (
+        check_directory_structure(
+            source=temp_git_dir / "src",
+            target=temp_git_dir / "tests",
+        )
+        == 0
+    )
