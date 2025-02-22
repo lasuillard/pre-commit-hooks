cdaf552c4f46ce8e802b7728efa55d84d000591a
Yuchan Lee <lasuillard@gmail.com>
2025-02-22 20:12:05 +0900

    Improve hook preferred-suffix (#58)

--- pre_commit_hooks/preferred_suffix.py
+++ pre_commit_hooks/preferred_suffix.py
@@ -3,15 +3,13 @@ from __future__ import annotations
 
 from enum import Enum
 from pathlib import Path  # noqa: TC003
-from typing import TYPE_CHECKING, NoReturn
+from typing import NoReturn
 
 import typer
 from rich import print  # noqa: A004
+from rich.table import Table
 
 from pre_commit_hooks.helpers.debugger import input_as_args
-
-if TYPE_CHECKING:
-    from collections.abc import Mapping
 
 app = typer.Typer()
 
@@ -21,15 +19,10 @@ class ExitCode(Enum):  # noqa: D101
     Error = 1
 
 
-suffixes_to_preferred: Mapping[str, str] = {
-    ".yml": ".yaml",
-}
-
-
-def preferred_suffix(*files: Path, rename: bool, dry_run: bool) -> ExitCode:  # noqa: D103
+def preferred_suffix(*files: Path, mapping: dict[str, str], rename: bool, dry_run: bool) -> ExitCode:  # noqa: D103
     violations: list[Path] = []
     for file in files:
-        preferred = suffixes_to_preferred.get(file.suffix)
+        preferred = mapping.get(file.suffix)
         if preferred is None:
             continue
 
@@ -40,7 +33,7 @@ def preferred_suffix(*files: Path, rename: bool, dry_run: bool) -> ExitCode:  # 
         print(f"❌ Found {len(violations)} file(s) with non-preferred suffix.")
         if rename:
             for file in violations:
-                preferred = suffixes_to_preferred[file.suffix]
+                preferred = mapping[file.suffix]
                 file_renamed = file.with_suffix(preferred)
                 if not dry_run:
                     file.rename(file_renamed)
@@ -60,6 +53,14 @@ def main(
         help="Files to check.",
     ),
     *,
+    mapping: list[str] = typer.Option(
+        ["yml -> yaml"],
+        help="List of suffix mapping, each in format of `suffix1,suffix2,... -> preferred-suffix`.",
+    ),
+    extend_mapping: list[str] = typer.Option(
+        [],
+        help="List of suffix mapping to extend the default mapping.",
+    ),
     rename: bool = typer.Option(
         False,  # noqa: FBT003
         help="Whether to rename files with preferred suffix automatically.",
@@ -70,8 +71,13 @@ def main(
     ),
 ) -> NoReturn:
     """Check filenames to use single preferred suffix over other possible variants."""
+    mapping_dict = _parse_mapping(mapping + extend_mapping)
+    print("📝 Mapping of suffixes to preferred suffix:")
+    _print_mapping(mapping_dict)
+
     exit_code = preferred_suffix(
         *files,
+        mapping=mapping_dict,
         rename=rename,
         dry_run=dry_run,
     )
@@ -79,7 +85,34 @@ def main(
     raise typer.Exit(exit_code.value)
 
 
-def entrypoint() -> None:  # noqa: D103
+def _parse_mapping(mapping: list[str]) -> dict[str, str]:
+    """Parse list of mapping string (`"suffix1,suffix2,... -> suffix"`) to dictionary of suffix to preferred suffix."""
+    result = {}
+    for item in mapping:
+        suffix_csv, preferred = item.split("->")
+        suffix_list = [f".{suffix.strip()}" for suffix in suffix_csv.split(",")]
+        preferred = f".{preferred.strip()}"
+        for suffix in suffix_list:
+            result[suffix] = preferred
+
+    return result
+
+
+def _print_mapping(mapping: dict[str, str]) -> None:
+    # Group suffixes to preferred suffix
+    match: dict[str, list[str]] = {}
+    for suffix, preferred in mapping.items():
+        match.setdefault(preferred, []).append(suffix)
+
+    # Print table in pretty format
+    table = Table("Preferred", "Suffixes")
+    for preferred, suffixes in match.items():
+        table.add_row(preferred, ", ".join(suffixes))
+
+    print(table)
+
+
+def entrypoint() -> None:  # noqa: D103  # pragma: no cover
     app()
 
 
